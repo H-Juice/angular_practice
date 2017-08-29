d89120dc1c5d9af9bf541d3d34162e103335c656
JenYata <godwn0317@gmail.com>
2017-08-29 16:01:45 +0900

    Add service component
    The @Injectable() decorator tells TypeScript to emit metadata about the service. The metadata specifies that Angular may need to inject other dependencies into this service.
    
    Don't use new with the HeroService
    
    To teach the injector how to make a HeroService, add the following providers array property to the bottom of the component metadata in the @Component call.
    
    The ngOnInit lifecycle hook
    Write an ngOnInit method with the initialization logic inside. Angular will call it at the right time. In this case, initialize by calling getHeroes().
    
    Async services and Promises
    Pass the callback function as an argument to the Promise's then() method:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,7 @@
 import { Component } from '@angular/core';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import {Hero} from "./hero";
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 
 @Component({
@@ -75,18 +65,21 @@ const HEROES: Hero[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
   onSelect(hero): void{
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
